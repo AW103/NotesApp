9ee2213bea4c5cbe50f0d91c30145a4576be598f
Andrea Walsh <andreawalsh@Carters1MacBook.attlocal.net>
2022-06-08 14:43:21 -0500

    testing

--- src/main/java/com/devmountain/noteapp/NoteAppApplication.java
+++ src/main/java/com/devmountain/noteapp/NoteAppApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 //@CrossOrigin(origins = "http://localhost:3000")
-@RestController
+//@RestController
 @SpringBootApplication
 public class NoteAppApplication {
 
-	@GetMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
+//	@GetMapping("/")
+//	@ResponseBody
+//	String home() {
+//		return "Hello World!";
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoteAppApplication.class, args);
--- src/main/java/com/devmountain/noteapp/controllers/UserController.java
+++ src/main/java/com/devmountain/noteapp/controllers/UserController.java
@@ -4,20 +4,23 @@ import com.devmountain.noteapp.dtos.UserDto;
 import com.devmountain.noteapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-//@RequestMapping("/")
+@RequestMapping("/")
 public class UserController {
     @Autowired
     private UserService userService;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+//    @GetMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     @PostMapping("/register")
     public List<String> addUser(@RequestBody UserDto userDto) {
